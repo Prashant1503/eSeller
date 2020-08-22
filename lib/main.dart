442943bfdda5f7060f625c3e1395f97651ebdb1d
Prashant1503 <prashant1998vani@gmail.com>
2020-08-22 13:16:29 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'app_sign_in/SignInPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Time Tracker',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: SignInPage(),
+    );
+  }
+}
+
+//
